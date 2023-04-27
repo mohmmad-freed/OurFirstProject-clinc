a24a6ea71474b84165d619604b411077e34fc99b
mohammad_fareed <mbdalmnm05@gmial.com>
2023-04-27 21:54:48 +0300

    Upload the project

---
+++ MASSENGER/src/main/java/com/amlehstation/massenger/MASSENGER.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.amlehstation.massenger;
+
+
+
+/**
+ *
+ * @author ASD
+ */
+public class MASSENGER {
+
+    public static void main(String[] args) {
+        LOGIN_SCREEN L1= new LOGIN_SCREEN();
+        L1.show();
+    }
+}
