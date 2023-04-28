98e375d89c170c49a847f514573da340b9cf5e39
m-q000 <msamalq306@gmail.com>
2023-04-28 22:55:21 +0300

    create class Person

---
+++ MASSENGER/src/main/java/com/amlehstation/massenger/PERSON.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.amlehstation.massenger;
+
+/**
+ *
+ * @author msamalq
+ */
+public class PERSON {
+    private String Name ,Pass;
+    private int Phone;
+
+    public void setName(String Name) {
+        this.Name = Name;
+    }
+
+    public void setPass(String Pass) {
+        this.Pass = Pass;
+    }
+
+    public void setPhone(int Phone) {
+        this.Phone = Phone;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public String getPass() {
+        return Pass;
+    }
+
+    public int getPhone() {
+        return Phone;
+    }
+    
+
+
+    
+}
